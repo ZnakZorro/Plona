4efd545733fd5a3c164b4c98ea0d59a66f2a0d4d
Znak Zorro <znakzorro@gmail.com>
2015-12-03 06:45:07 +0100

    re new radios sh

---
+++ renewradios.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+mpc clear
+mpc add http://mainstream.radioagora.pl:80/tuba9-1.mp3
+mpc add http://31.192.216.5:8000/rmf_classic
+mpc add http://pub4.jazzradio.com:80/jr_jazzballads?d1e1bfa0a67fafc4764954be
+mpc add http://pub2.jazzradio.com:80/jr_pianotrios?d1e1bfa0a67fafc4764954be
+mpc add http://pub5.jazzradio.com:80/jr_vocallegends?d1e1bfa0a67fafc4764954be
+mpc add http://pub1.jazzradio.com:80/jr_gypsyjazz?d1e1bfa0a67fafc4764954be
+mpc add http://pub6.jazzradio.com:80/jr_guitarjazz?d1e1bfa0a67fafc4764954be
+mpc add http://streaming.radionomy.com/Acoustic-Guitar
+mpc add mms://stream.polskieradio.pl/program1
+mpc add mms://stream.polskieradio.pl/program2
+mpc add mms://stream.polskieradio.pl/program3
+mpc add mms://stream.polskieradio.pl/program4
+mpc add http://stream4.nadaje.com:11986/prs.aac
+mpc add http://streaming.radionomy.com/Sax4Love
+mpc add http://50.7.173.162:8014/
+mpc add http://50.7.173.162:8012/
+mpc add http://listen.radionomy.com/the-smooth-lounge
+mpc add http://mainstream.radioagora.pl/tuba9-1.mp3
+mpc add http://178.32.161.112:5700
+mpc add http://91.121.92.167:8700
+mpc add http://91.121.92.167:8900
+mpc add http://178.32.161.112:9100
+mpc add http://178.32.161.113:9200
+mpc add http://mp3.polskieradio.pl:8020
