ca5ae050e2dc47fd7bcd06acf0d115b528c2d6a7
Znak Zorro <znakzorro@gmail.com>
2015-12-04 08:11:03 +0100

    add volume and info

--- express_server_example.js
+++ express_server_example.js
@@ -1,17 +1,81 @@
+var loopnr=0;
+var lastremoteIP='';
 var express = require('express');
 var app = express();
+
+var util = require('util'),
+    exec = require('child_process').exec,
+    child;
+var infor = '';
+	
+	function graj(r){
+	//infor = '';
+		child = exec('mpc play '+r,
+		  function (error, stdout, stderr) {
+			//console.log(stdout);
+			infor = stdout;
+			//console.log('stdout: ' + stdout);
+			//console.log('stderr: ' + stderr);
+			if (error !== null) {
+			  console.log('exec error: ' + error);
+			infor = error;
+			}
+		});
+
+	}
+
+	function glos(r){
+		if (isNaN(r)) return;
+		child = exec('mpc volume '+r,
+		  function (error, stdout, stderr) {
+			infor = stdout;
+			if (error !== null) {console.log('exec error: ' + error); infor = error;}
+		});
+	}
+
+
+var os = require( 'os' );
+var networkInterfaces = os.networkInterfaces( );
+//console.log( networkInterfaces );
+var localIP = networkInterfaces.wlan1[0]['address'];
+console.log('localIP=',localIP);
+
+
+
+
 var nr=0;
 app.get('/', function (req, res) {
+
+	var remoteIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	if(remoteIP !=lastremoteIP){		
+		console.log('remoteIP=',remoteIP);
+		lastremoteIP = remoteIP;
+	}
+	console.log('req.query=',req.query);
 	var czas = (new Date()).toLocaleString();
 	var paramRadio = req.query.radio;
+	var paramInfo  = req.query.info;
 	var paramBash  = req.query.bash;
+	var paramVol   = req.query.volume;
 	var czas = (new Date()).toLocaleString();
 	var arr ={};
-	arr.wiadomosc="to ju Hello";
+	//arr.wiadomosc="";
 	arr.nr = nr;
 	arr.czas = czas;
-	if (paramRadio) arr.radio = paramRadio;
+	if (paramRadio){
+		if(isNaN(paramRadio)){
+			var vok = parseInt(paramRadio.slice(1));
+			if(vok != 'NaN') glos(vok);
+		}
+		else if (paramRadio) {
+			arr.radio = paramRadio;
+			graj(paramRadio);
+		}
+		arr.wiadomosc = infor;
+	} // if paramRadio
+
 	if (paramBash)  arr.bash = paramBash;
+	if (paramVol)   arr.vol = paramVol;
 	var json = JSON.stringify(arr);
 	
 	res.setHeader("Content-Type", "application/json");
@@ -20,13 +84,12 @@ app.get('/', function (req, res) {
 	
 	res.send(json);
 	nr++;
-
+	console.log(json);
 });
 
-var server = app.listen(1956, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+	var server = app.listen(8080, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+	//console.log(server.address());
+	console.log('Express radiodom listening at http://%s, http://%s:%s', localIP, host, port);
 });
-
